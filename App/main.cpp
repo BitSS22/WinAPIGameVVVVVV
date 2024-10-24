e89ad7373b367acbfa275dd29c9445a13c1f73dc
Bitss <donrang@naver.com>
2024-10-24 13:41:41 +0900

    오류 수정

--- App/main.cpp
+++ App/main.cpp
@@ -1,9 +1,13 @@
 #include "pch.h"
 #include <EngineCore/EngineAPICore.h>
+#include <VVVVVV/V6ContentsCore.h>
 
 #pragma comment (lib, "EngineCore.lib")
+#pragma comment (lib, "VVVVVV.lib")
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
-	return UEngineAPICore::EngineStart(hInstance);
+	V6ContentsCore User = {};
+
+	return UEngineAPICore::EngineStart(hInstance, &User);
 }
